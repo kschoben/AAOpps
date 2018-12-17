a55953e606bacb6173e4077240344d54b825a9e7
Kevin Schoben <kevinschoben@gmail.com>
2018-12-16 23:40:09 -0800

    Layout Additions

--- src/App.js
+++ src/App.js
@@ -1,51 +1,80 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Layout, Menu, Breadcrumb, Dropdown, Button} from 'antd';
+import { Layout, Menu, Breadcrumb, Dropdown, Button, Input, Collapse,Select,Icon} from 'antd';
 
 const { Header, Content, Footer } = Layout;
-
-const menu = (
-  <Menu>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
-    </Menu.Item>
-  </Menu>
-);
+const Option = Select.Option;
+const Panel = Collapse.Panel;
+const customPanelStyle = {
+  background: 'gray',
+  borderRadius: 4,
+  marginBottom: 24,
+  paddingBottom:2,
+  border: 0,
+  overflow: 'hidden',
+};
 
 class App extends Component {
   render() {
     return (
       <Layout className="layout">
       <Header>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Dropdown overlay={menu} placement="bottomLeft">
-              <Button>bottomLeft</Button>
-          </Dropdown>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+          </Menu>
+        </Header>
       <Content style={{ padding: '0 50px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
         </Breadcrumb>
-        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+        <div style={{ background: '#fff', padding: 24 }}>
+          <div>
+            <Input addonBefore="Manual Title:" defaultValue="" />
+          </div>  
+          <div>
+             <Input addonBefore="Manual SubTitle:" defaultValue="" />
+          </div>  
+            <Collapse accordion defaultActiveKey={['1']}>
+              <Panel header="Untitled" key="1" style={customPanelStyle}>
+                <div style={{ marginBottom: 16 }}>
+                  <Input addonBefore="Section Title:" defaultValue="" />
+                </div>
+                <Collapse accordion defaultActiveKey={['1']}>
+                <Panel header="Untitled" key="1" style={customPanelStyle}>
+                  <div style={{ marginBottom: 16 }}>
+                    <Input addonBefore="Section Title:" defaultValue="" />
+                  </div>
+                  <div>
+                    <AddSectionButton/>
+                    <AddGroupButton/>
+                    <AddTextButton/>
+                    <AddTableButton/>
+                  </div>
+                </Panel>
+              </Collapse>
+                <div>
+                  <AddSectionButton/>
+                  <AddGroupButton/>
+                  <AddTextButton/>
+                  <AddTableButton/>
+                </div>
+              </Panel>
+            </Collapse> 
+            <div>
+              <AddSectionButton/>
+              <AddGroupButton/>
+              <AddTextButton/>
+              <AddTableButton/>
+            </div>
+          </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>
         Ant Design ©2018 Created by Ant UED
@@ -55,4 +84,54 @@ class App extends Component {
   }
 }
 
+class AddTextButton extends React.Component {
+  addText (){
+      alert ('Text')
+  }
+  render() {
+      return (
+        <Button type="dashed" onClick = {this.addText}>
+          <Icon type="plus" /> Add Text
+        </Button>
+      );
+  }
+}
+
+class AddGroupButton extends React.Component {
+  addText (){
+      alert ('Group')
+  }
+  render() {
+      return (
+        <Button type="dashed" onClick = {this.addText}>
+          <Icon type="plus" /> Add Group
+        </Button>
+      );
+  }
+}
+
+class AddSectionButton extends React.Component {
+  addText (){
+      alert ('Section')
+  }
+  render() {
+      return (
+        <Button type="dashed" onClick = {this.addText}>
+          <Icon type="plus" /> Add Section
+        </Button>
+      );
+  }
+}
+class AddTableButton extends React.Component {
+  addText (){
+      alert ('Table')
+  }
+  render() {
+      return (
+        <Button type="dashed" onClick = {this.addText}>
+          <Icon type="plus" /> Add Table
+        </Button>
+      );
+  }
+}
 export default App;
